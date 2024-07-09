d33af34f4fc3b870849827c8e84d824dd001c791
Md Mahadi Hasan <mahadib2b@gmail.com>
2024-07-09 22:55:49 +0600

    on details variable

--- Dart_Variable.dart
+++ Dart_Variable.dart
@@ -1,6 +1,9 @@
-void main(){
-  var x =10;  // (x variable define)(= assignment operator) (10 operands this is value int (int =only number))
-  var y =20;
-  var z = x +y;
-  print(z);
+
+
+void main(){           //void main() {} this is main function  to run the application
+  var x =10;          // (x variable define)(= assignment operator) (10 operands this is value int (int =only number))
+  var y =20;          // ; end line
+  var z = x +y;       // x+y concrete
+  print(z);           // print(); output
+  print('My age is $y & My roll no $x'); // string interpolation
 }
