1078bc0bda5c4ec5621fb902d93cb43c2ca36bfa
Md Mahadi Hasan <mahadib2b@gmail.com>
2024-07-09 23:30:49 +0600

    Type safty data type

--- Dart_Variable.dart
+++ Dart_Variable.dart
@@ -5,5 +5,14 @@ void main(){           //void main() {} this is main function  to run the applic
   var y =20;          // ; end line
   var z = x +y;       // x+y concrete
   print(z);           // print(); output
-  print('My age is $y & My roll no $x'); // string interpolation
+  print('My age is $y & My roll no $x\n\n');   // string interpolation &\n new line define & $are variable define
+
+  //Type of Data safty
+  String h ='I am Mahadi Hasan 202'; //String anyvariable(first letter small) = "" or '' ;
+  int i = 10; // only int number
+  double j = 10.5; // only double number
+  bool k = true ; // only true and false
+
+  print("Inter Your full Details \n$h \nI am $i Years old \nMy weight $j \nAll the information are $k");
+
 }
