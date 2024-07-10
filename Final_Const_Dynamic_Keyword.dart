78d073d17ac8446fb1cd6aa48b9365a5ba25a915
Md Mahadi Hasan <mahadib2b@gmail.com>
2024-07-10 23:23:32 +0600

    final & const

---
+++ Final_Const_Dynamic_Keyword.dart
@@ -0,0 +1,13 @@
+void main(){
+  var a =10;
+  int b =15;
+      b =20;
+
+  dynamic c =30;
+          c =35;
+
+   final d = 50;
+   //d =20;  no decaler any value
+  const e = 60;
+  // e =70; no decaler any value
+}
