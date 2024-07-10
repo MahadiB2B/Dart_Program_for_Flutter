26ce662c13f5cbcc71e186460e3c12e1dc33cc23
Md Mahadi Hasan <mahadib2b@gmail.com>
2024-07-10 23:09:39 +0600

    dart syntex & dart variable

---
+++ Dart_Syntax.dart
@@ -0,0 +1,7 @@
+/*
+Whitespace and Line Breaks - Ignores
+Dart is case-sensitive - Uper & Lower not same
+Statements end with a Semicolon - ;
+Comments in Dart - Single line comments // & Multi Line /* ...*/
+Dart keywords  https://dart.dev/language/keywords
+*/
