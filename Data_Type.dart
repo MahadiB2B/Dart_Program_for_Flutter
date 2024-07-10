b8c6a7f8a364692fd2ded0fa10aed623d6aa00cf
Md Mahadi Hasan <mahadib2b@gmail.com>
2024-07-10 23:55:44 +0600

    dart operator

---
+++ Data_Type.dart
@@ -0,0 +1,26 @@
+/*
+**Number
+   int - example > 10;
+   double / float - example > 10.00;
+
+** String
+    String - example > "am Mahadi 50" or
+             example > '50.4'
+
+** Boolean
+  True & false
+
+** List
+var city = ['Dhaka','Bogura', 'Rangpur', '10', '10.0', 'true'];
+ print (city[2]);
+
+
+** Maps
+ var city = {'Dhaka':'Motijhil', 'Bogura':'Belail', 'post':'5800'};
+  print (city['Dhaka']);
+
+**Runes
+
+** Symbols
+
+* */
