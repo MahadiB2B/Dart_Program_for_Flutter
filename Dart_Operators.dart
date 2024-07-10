d757996f018be38547c778d358059e726728e5e5
Md Mahadi Hasan <mahadib2b@gmail.com>
2024-07-10 23:55:44 +0600

    List add addAll

---
+++ Dart_Operators.dart
@@ -0,0 +1,17 @@
+// https://dart.dev/language/operators
+
+/* unary operators example
+++ (prefix)  ++x
+++ (postfix) x++
+-- (prefix)  --x
+-- (postfix) X--
+
+postfix                 prefix
+var x =10;              var x =10;
+ output                    output
+ 10                     11
+ 11                     12
+ 12                     13
+ 13                     14
+
+ */
---
+++ Growable_List.dart
@@ -0,0 +1,18 @@
+// Growable list
+void main(){
+  var mylist = [1,2,3,4,5];
+  print (mylist);  // list
+
+  //add
+  mylist.add(6);
+  mylist.add(7);
+  print(mylist);
+
+  //addAll
+  mylist.addAll([8,9,10]);
+  print(mylist);
+
+  //Insert
+
+
+}
