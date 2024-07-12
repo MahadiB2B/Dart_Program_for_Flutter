c8d43db377f294be976c32bc9879acea5a47ad66
Md Mahadi Hasan <mahadib2b@gmail.com>
2024-07-12 08:56:40 +0600

    fixed length list

---
+++ Fixed_Length_List.dart
@@ -0,0 +1,9 @@
+void main(){
+  final list = <String>['0','1','2','3','4',];
+  list[0] = 'I';
+  list[1] = 'love';
+  list[2] = 'You';
+  list[3] = 'Bangladesh';
+  print(list);
+}
+// Fixed length list length can't be changed or increased
