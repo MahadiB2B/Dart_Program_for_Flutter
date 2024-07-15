c4ef5b4affd1ea5db91bb3650b5eb26fac40da31
Md Mahadi Hasan <mahadib2b@gmail.com>
2024-07-15 21:53:54 +0600

    if else example 4

--- Dart_Control_flow_Statement/Dicision-Making_Statement/if-else.dart
+++ Dart_Control_flow_Statement/Dicision-Making_Statement/if-else.dart
@@ -16,7 +16,7 @@ void main() {
   } */
 
 
-  //if, else & else-if
+  //if, else & else-if Example-1 (school marks)
 var marks = 60;
 if (marks >=80 && marks<=100){print('A+');}
 else if (marks >=70 && marks <80){print('A');}
@@ -25,4 +25,30 @@ else if (marks >=50 && marks <60){print('B');}
 else if (marks >=40 && marks <50){print('c');}
 else if (marks >=33 && marks <40){print('D');}
 else {print('Fail');}
+
+//if, else & else-if Example-2 (7 day)
+var day = 3;
+if (day ==1) {print('Saturday');}
+else if (day ==2){print('Sunday');}
+else if (day ==3){print('Monday');}
+else if (day ==4){print('Tuesday');}
+else if (day ==5){print('Wednesday');}
+else if (day ==6){print('Thursday');}
+else if (day ==7){print('Friday');}
+else {print('Invalid day');}
+
+//if, else & else-if Example-3 (Age Adult & Child)
+var age =23;
+if (age <=17){print('Child');}
+else if (age >=24){print('Adult');}
+else {print('Invalid');}
+
+
+
+
+
 }
+
+
+
+
