0b96712362eca58336890eab668a9d7b2434fc41
Md Mahadi Hasan <mahadib2b@gmail.com>
2024-07-23 08:40:59 +0600

    getter method

---
+++ Class/Setter_Getter.dart
@@ -0,0 +1,25 @@
+class Worker{
+  String company = 'ABC';
+  String designation = 'Softwar Engineer';
+  double _salary = 30000;
+
+  //getter
+  /*
+  double getSalary(){
+    return _salary;
+  } */
+
+/*
+  double get getSalary{
+    return _salary;
+  }
+
+ */
+
+double get salary{
+  return _salary;
+}
+
+//Setter
+
+}
---
+++ Class/Setter_Getter_main.dart
@@ -0,0 +1,11 @@
+import 'Setter_Getter.dart';
+
+void main(){
+  Worker mahadi = Worker();
+  mahadi.company;
+  mahadi.designation;
+
+ //print(mahadi.getSalary());  //()
+ // print(mahadi.getSalary);
+print(mahadi.salary);
+}
